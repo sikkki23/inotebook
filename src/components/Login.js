1fa83f8493acbf5815b0d27913d42297ee94ea27
sicky <chandra.23.jai@gmail.com>
2025-03-06 10:08:04 +0530

    implement api call for login and signup

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,27 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react';
+import  { useNavigate  } from 'react-router-dom';
 
-const Login = () => {
-    const handleSubmit = (e) => {
+const Login =  () => {
+
+    const [credentials, setCredentials] = useState({email: "",password: ""});
+    let navigate = useNavigate();
+    const handleSubmit = async (e) => {
         e.preventDefault();
+
+        let bodydata = JSON.stringify({
+            email:credentials.email,
+            password:credentials.password  
+          });
+        const response= await fetch(`http://localhost:5000/api/auth/login`, {
+            method : 'POST',
+            headers: {
+              'content-type' : 'application/json',
+              "auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjdjNjZiNDQzNmJlNzYxZDU1NzhkODVlIn0sImlhdCI6MTc0MTIzMjQxMn0.FCnGygvSlwHjyglxhYV-eOUrcgoUiTr1lBYPkRJ0484"
+            }, body: bodydata
+      });
+      console.log("body", bodydata);
+      const json = await response.json();
+      console.log("This is getNotes ",json);
+      if(json.success)
+      {
+        localStorage.setItem('token',json.authToken);
+        navigate("/");
+      }else{
+        alert("plz check credentials");
+      }
     }
+
+    const onChange = (e) => {
+        setCredentials({...credentials,[e.target.name]: e.target.value})
+      }
+
 
     return (
         <div>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <div className="mb-3">
-                    <label for="exampleInputEmail1" className="form-label">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" value={credentials.email} onChange={onChange}/>
                     <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div className="mb-3">
-                    <label for="exampleInputPassword1" className="form-label">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"  name="password" value={credentials.pasword} onChange={onChange}/>
                 </div>
                 <div className="mb-3 form-check">
                     <input type="checkbox" className="form-check-input" id="exampleCheck1" />
-                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                    <label className="form-check-label" htmlFor="exampleCheck1">Check me out</label>
                 </div>
-                <button type="submit" className="btn btn-primary" onSubmit={handleSubmit}>Submit</button>
+                <button type="submit" className="btn btn-primary" >Submit</button>
             </form>
         </div>
     )
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,8 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react';
+import  { useNavigate  } from 'react-router-dom';
 
-function Signup() {
+const Signup = () => {
+
+  const [credentials, setCredentials] = useState({name : "",email: "",password: "",cpassword: ""});
+  let navigate = useNavigate();
+  const {name,email,password} = credentials;
+  const handleSubmit = async (e) => {
+      e.preventDefault();
+
+      let bodydata = JSON.stringify({
+          name:name,
+          email:email,
+          password:password  
+        });
+      const response= await fetch(`http://localhost:5000/api/auth/createuser`, {
+          method : 'POST',
+          headers: {
+            'content-type' : 'application/json',
+            "auth-token" : "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjdjNjZiNDQzNmJlNzYxZDU1NzhkODVlIn0sImlhdCI6MTc0MTIzMjQxMn0.FCnGygvSlwHjyglxhYV-eOUrcgoUiTr1lBYPkRJ0484"
+          }, body: bodydata
+    });
+    console.log("body", bodydata);
+    const json = await response.json();
+    console.log("This is getNotes ",json);
+    if(json.success)
+    {
+      localStorage.setItem('token',json.authToken);
+      navigate("/");
+    }else{
+      alert("plz check credentials");
+    }
+  }
+
+  const onChange = (e) => {
+      setCredentials({...credentials,[e.target.name]: e.target.value})
+    }
+
+
   return (
-    <div>I am signup</div>
+    <div>
+      <div>
+            <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                    <label htmlFor="exampleInputName" className="form-label">Name</label>
+                    <input type="text" className="form-control" id="name" aria-describedby="nameHelp" name="name" required minLength={5} value={credentials.name} onChange={onChange}/>
+                    </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputEmail1" className="form-label">Email address</label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" name="email" required minLength={5} value={credentials.email} onChange={onChange}/>
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1"  name="password" required minLength={5} value={credentials.pasword} onChange={onChange}/>
+                </div>
+                <div className="mb-3">
+                    <label htmlFor="exampleInputPassword1" className="form-label">Confirm Password</label>
+                    <input type="password" className="form-control" id="cexampleInputPassword1"  name="cpassword" required minLength={5} value={credentials.pasword} onChange={onChange}/>
+                </div>
+                <button type="submit" className="btn btn-primary" >Submit</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
